89cdbc07c0a480c015ab8fd154e275714d1001f1
samrat-gh <samratghimire32@gmail.com>
2024-08-22 21:38:33 +0545

    intial Setup with Sidebar

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Nepse Portfolio Management System",
+  description: "See your Financial Growth Anywhere",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "",
+    github: "",
+    docs: "",
+  },
+}
