9dec2e78627b1b230b6a64ba6f7483dcfed9dfcb
Samrat Ghimire <samratghimire32@gmail.com>
2025-01-27 23:43:02 +0545

    refactor and update to green theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import { Metadata, Viewport } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout"
-import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
